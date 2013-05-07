90bb8a84f0b96a0abb261e14469e42678d0bdb00
AlienEngineer <Alien.Software.Engineer@gmail.com>
2013-05-07 10:51:34 +0100

    Inicial project setup

---
+++ PipelineRunner.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace PipelineRunner.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
